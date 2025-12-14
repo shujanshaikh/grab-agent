72637a6e42863062ff150079e411b14859d35aa7
Shujan Shaikh <shaikhshujan@gmail.com>
2025-12-14 12:56:50 +0530

    minor fix

--- packages/grab-it/src/constant.ts
+++ packages/grab-it/src/constant.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_SERVER_URL = "wss://grab-agent-server-production.up.railway.app";
-export const SERVER_URL = "https://grab-agent-server-production.up.railway.app"
+export const DEFAULT_SERVER_URL = "wss://grab-server-production.up.railway.app";
+export const SERVER_URL = "https://grab-server-production.up.railway.app"
